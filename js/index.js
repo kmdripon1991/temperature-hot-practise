3cb4a1c3bdbdb4434543d52ef4d0bcc9ecf3abf9
kmdripon1991 <kmdripon1991@gmail.com>
2023-09-03 17:05:06 +0600

    commit

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ const loadTemperature = (city) =>{
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     fetch(url)
     .then(res =>res.json())
-    .then(data => displayTemperature(data))
+    .then(data => showTemperature(data))
 }
 
-const displayTemperature = data =>{
+const showTemperature = data =>{
     const temperature = document.getElementById('temperature')
     temperature.innerText = data.main.temp
     console.log(data.main)
