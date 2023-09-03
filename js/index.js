54690162cb7f64c5d0f0126b735477eea9310261
kmdripon1991 <kmdripon1991@gmail.com>
2023-09-03 23:49:59 +0600

    practise done

--- js/index.js
+++ js/index.js
@@ -1,16 +1,30 @@
-const API_KEY = `8dbfcb15a835dfdfded19e364beedbdb`
+const API_KEY = `...`
 
 const loadTemperature = (city) =>{
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    console.log(url)
     fetch(url)
     .then(res =>res.json())
     .then(data => showTemperature(data))
 }
 
 const showTemperature = data =>{
-    const temperature = document.getElementById('temperature')
-    temperature.innerText = data.main.temp
-    console.log(data.main)
+    setInnerText('temperature', data.main.temp);
+    setInnerText('condition', data.weather[0].main);
+    // console.log(data.weather[0].main)
 }
 
-loadTemperature('dhaka')
+
+
+const tempSearch = () =>{
+    const inputText = document.getElementById('input-text')
+    const city = inputText.value
+    document.getElementById('city').innerText = city
+    loadTemperature(city)
+}
+
+const setInnerText = (id,text) =>{
+    document.getElementById(id).innerText = text;
+}
+
+loadTemperature()
